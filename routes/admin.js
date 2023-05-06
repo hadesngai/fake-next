68fe61fdd957327103ceed79ffb6440497c09175
ngaikakwan <41099782+ngaikakwan@users.noreply.github.com>
2023-05-06 19:20:14 +0800

    restructure

--- routes/admin.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET admin management page. */
-router.get('/admin', function(req, res) {
-  res.render('admin', { title: 'Admin Interface' });
-});
-
-module.exports = router;
--- routes/pikachu.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET secret page. */
-router.get('/pikachu', function(req, res) {
-  res.render('pikachu', { title: 'Hey' });
-});
-
-module.exports = router;
